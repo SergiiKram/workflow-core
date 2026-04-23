7e72917fd2a3179fb48bc0b9a56b6c41f5f00a58
Sergii Kram <sergiikram@outlook.com>
2026-04-23 16:01:28 +1000

    Critical updates

--- src/WorkflowCore.DSL/Services/Deserializers.cs
+++ src/WorkflowCore.DSL/Services/Deserializers.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using Newtonsoft.Json;
-using SharpYaml.Serialization;
+﻿using Newtonsoft.Json;
+using SharpYaml;
+using System;
 using WorkflowCore.Models.DefinitionStorage.v1;
 
 namespace WorkflowCore.Services.DefinitionStorage
 {
     public static class Deserializers
     {
-        private static Serializer yamlSerializer = new Serializer();
-
         public static Func<string, DefinitionSourceV1> Json = (source) => JsonConvert.DeserializeObject<DefinitionSourceV1>(source);
 
-        public static Func<string, DefinitionSourceV1> Yaml = (source) => yamlSerializer.DeserializeInto(source, new DefinitionSourceV1());
+        public static Func<string, DefinitionSourceV1> Yaml = (source) => YamlSerializer.Deserialize<DefinitionSourceV1>(source);
     }
 }
